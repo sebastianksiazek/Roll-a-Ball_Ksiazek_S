c9049a7c2fc1d77c360d3159c678a10325198117
SebastianK <29943@s.pm.szczecin.pl>
2024-10-17 12:54:47 +0200

    1 commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraContoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 offset;
+    public GameObject Player;
+    void Start()
+    {
+        offset = Player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+        transform.position = Player.transform.position - offset;
+
+    }
+}
