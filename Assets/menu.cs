9ac5d7a4c3a9c81677223cb52982259e4fcefc46
SebastianK <29943@s.pm.szczecin.pl>
2024-10-24 12:59:16 +0200

    add menu

---
+++ Assets/menu.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    public void ShowOptions()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
