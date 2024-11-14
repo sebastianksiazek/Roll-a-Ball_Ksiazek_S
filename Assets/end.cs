94429e41e1afbdac5cead689798fcf95afc37683
SebastianK <29943@s.pm.szczecin.pl>
2024-11-14 11:58:28 +0100

    add

--- Assets/end.cs
+++ Assets/end.cs
@@ -6,26 +6,15 @@ public class end : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
-    void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-
-        //scoreValue += 1;
-        collision.gameObject.GetComponent<MovementController>().scoreValue += 1;
-        //Debug.Log("Score: " + collision.gameObject.GetComponent<MovementController>().scoreValue);
-        gameObject.SetActive(false);
-        if (collision.gameObject.GetComponent<MovementController>().scoreValue == 1)
-        {
-            Debug.Log("You win!");
-            SceneManager.LoadScene(3, LoadSceneMode.Single);
-        }
-
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
