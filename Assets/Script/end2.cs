7ebf935dfcf44b777d93146fe060b5510f832076
Sebastian Ksiazek <29943@s.pm.szczecin.pl>
2025-01-19 18:11:37 +0100

    add lvl

--- Assets/Script/end2.cs
+++ Assets/Script/end2.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class end : MonoBehaviour
+public class end2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -10,7 +10,7 @@ public class end : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(4, LoadSceneMode.Single);
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
     }
 
     // Update is called once per frame
